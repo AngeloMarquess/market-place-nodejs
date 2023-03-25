1879f3df667f338c972d4d800406fc8438d5641d
Angelo Marques <angelofe@gmail.com>
2023-03-25 13:46:46 -0300

    Feat: criptografia na senha do user

--- src/model/Usuario.js
+++ src/model/Usuario.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const UsuarioSchema = new mongoose.Schema({
 nome: {type: String, required: true},
@@ -25,6 +26,12 @@ createAt: {type: Date, required: true , default:Date.now()},
 // admin:{ type: Boolean, required: true, default:false},
 });
 
+UsuarioSchema.pre("save",async function(next){
+  if(this.senha){
+    this.senha =await bcrypt.hash(this.senha,10);
+  }
+  next();
+});
 const Usuario = mongoose.model("usuarios",UsuarioSchema);
 
 module.exports = Usuario;
